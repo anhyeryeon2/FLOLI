66fc9a174f8659b6d90c194ceed172a7ee0ce5b6
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-01-05 16:46:48 +0900

    feat:header,navber 숨기기처리-router #24

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,9 @@
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import styled from 'styled-components'
 import Header from '../header/header-main/Header'
 import Navbar from '../navbar/Navbar'
 
-const Container = styled.div`
+export const Container = styled.div`
   max-width: var(--max-width);
   min-height: 100vh;
   margin: 0 auto;
@@ -11,26 +11,11 @@ const Container = styled.div`
 `
 
 const Layout = () => {
-  const location = useLocation()
-  let shouldHideHeader = false
-  let shouldHideNavbar = false
-
-  switch (location.pathname) {
-    case '/login':
-    case '/signup':
-      shouldHideHeader = true
-      shouldHideNavbar = true
-      break
-    default:
-      shouldHideHeader = false
-      shouldHideNavbar = false
-  }
-
   return (
     <Container>
-      {!shouldHideHeader && <Header />}
+      <Header />
       <Outlet />
-      {!shouldHideNavbar && <Navbar />}
+      <Navbar />
     </Container>
   )
 }
