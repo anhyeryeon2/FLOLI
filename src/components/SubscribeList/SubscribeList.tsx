02d45c2c125569a11401f7ad96034d9b01af9562
rondido <rppr001@naver.com>
2025-01-05 18:35:59 +0900

    feat: 구독 목록 드래그 슬라이더 구현

--- src/components/SubscribeList/SubscribeList.tsx
+++ src/components/SubscribeList/SubscribeList.tsx
@@ -1,5 +1,7 @@
+import { useRef } from 'react'
 import * as S from './SubscribeList.module'
-
+import img from '../../assets/test.webp'
+import { useDragScroll } from '@/hooks/useDragScroll'
 const data = [
   {
     user_id: 'p5o4n3m2-l1k0j9-i8h7g6-f5e4d3c2b1a0',
@@ -7,7 +9,7 @@ const data = [
     nickname: 'JaneDoe456',
     created_at: '2025-02-01T08:00:00Z',
     updated_at: '2025-02-15T14:30:00Z',
-    profile_img_path: '/assets/img/profile/default_profile.webp',
+    profile_img_path: '../../assets/test.webp',
     password: 'hashedpassword456',
     introduction: 'Frontend developer and designer.',
     subsc_count: 10,
@@ -34,7 +36,7 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '23',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -50,7 +52,7 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '10',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -66,7 +68,7 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '6',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -82,7 +84,23 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '5',
+    email: 'bobjones@example.com',
+    nickname: 'BobTheBuilder',
+    created_at: '2025-03-01T08:00:00Z',
+    updated_at: '2025-03-20T09:00:00Z',
+    profile_img_path: '../../assets/test.webp',
+    password: 'hashedpassword789',
+    introduction: null,
+    subsc_count: 0,
+    subscription_status: 'expired', // 구독 만료
+    subscription_plan: 'free', // 무료 플랜
+    subscription_start_date: '2025-03-01T12:00:00Z',
+    subscription_end_date: '2025-03-31T23:59:59Z',
+    last_payment_date: null
+  },
+  {
+    user_id: '4',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -98,7 +116,7 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '3',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -114,7 +132,7 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '30',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -130,7 +148,7 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '2=399',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -146,7 +164,7 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '09',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -162,7 +180,7 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '60',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -178,7 +196,7 @@ const data = [
     last_payment_date: null
   },
   {
-    user_id: 'z1x2c3v4-b5n6-m7a8-s9d0-f1g2h3j4k5l6',
+    user_id: '555',
     email: 'bobjones@example.com',
     nickname: 'BobTheBuilder',
     created_at: '2025-03-01T08:00:00Z',
@@ -196,19 +214,33 @@ const data = [
 ]
 
 const SubscribeList = () => {
+  const scrollRef = useRef<HTMLDivElement | null>(null)
+
+  const { onDragStart, onDragEnd, isDrag, onThrottleDragMove } =
+    useDragScroll(scrollRef)
+
   return (
-    <S.SubscribeListContainer>
-      {data.map(value => (
-        <S.SubscribeListItem key={value.user_id}>
-          <img
-            src={value.profile_img_path}
-            alt="구독한 프로필 이미지"
-            width={35}
-            height={33}
-          />
-        </S.SubscribeListItem>
-      ))}
-    </S.SubscribeListContainer>
+    <S.SubscribeContainer>
+      <S.SubscribeListContainer
+        ref={scrollRef}
+        onMouseDown={onDragStart} // 마우스 클릭 시
+        onMouseUp={onDragEnd} // 마우스 클릭을 뗀 후
+        onMouseLeave={onDragEnd} //마우스가 해당 요소 영역을 벗어낫을 때
+        onMouseMove={isDrag ? onThrottleDragMove : undefined} //마우스가 움직일때
+      >
+        {data.map(value => (
+          <S.SubscribeListItem key={value.user_id}>
+            <S.SubscribeItem>
+              <S.SubscribeAvatar
+                src={img}
+                alt="구독한 프로필 이미지"
+              />
+            </S.SubscribeItem>
+          </S.SubscribeListItem>
+        ))}
+      </S.SubscribeListContainer>
+      <S.SubscribeAllList>전체</S.SubscribeAllList>
+    </S.SubscribeContainer>
   )
 }
 
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,12 +1,12 @@
 export { Login } from './login/index.tsx'
-export { Signup } from './Signup/index.tsx'
-export { NotFound } from './Notfound/index.tsx'
+export { Signup } from './signup/index.tsx'
+export { NotFound } from './notfound/index.tsx'
 
 export { PlaylistCreate } from './playListCreate/index.tsx'
 export { profileEdit } from './profileEdit/index.tsx'
 export { UserProfile } from './userProfile/index.tsx'
 
-export { Mypage } from './Mypage/index.tsx'
+export { Mypage } from './mypage/index.tsx'
 
 export { MyPlaylists } from './myPlayList/index.tsx'
 export { Subscriptions } from './subscriptions/index.tsx'
