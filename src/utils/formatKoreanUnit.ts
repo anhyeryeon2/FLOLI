949c6499938e278be568755d7a222e274b4c2374
KwonSeami <kwonsam07@daum.net>
2025-01-08 15:37:34 +0900

    feat: 단위 관련 util 함수 작업 #66

--- src/utils/formatKoreanUnit.ts
+++ src/utils/formatKoreanUnit.ts
@@ -1,10 +1,14 @@
 /**
  * 숫자를 한글 단위로 변환하여 포맷합니다.
  * @param num 변환할 숫자
- * @returns 변환된 숫자 문자열 (예: 25.6만 / 3천)
+ * @returns 문자열 (예: 25.6만 / 3천)
  * @example const test = formatKoreanUnit(256000); // "25.6만"
  */
 export function formatKoreanUnit(num: number): string {
+  if (!num) {
+    // num이 null일 때
+    return '0'
+  }
   if (num >= 10_000) {
     return (num / 10_000).toFixed(1) + '만'
   } else if (num >= 1_000) {
