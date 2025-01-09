d770014c12f7fc047d0a0e62e8624ae5dc68b44a
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-01-09 15:34:37 +0900

    feat: 소셜로그인 초기 구현

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,7 @@
 import { supabase } from '../../../supabaseConfig'
 import { useNavigate } from 'react-router-dom'
 import Input from '@/components/Input/Input'
-import { Button } from '@/components/Button/Button'
+import { Button } from '@/components/button/Button'
 import * as S from './login.styles'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -9,6 +9,7 @@ import { loginSchema, LoginFormInputs } from '@/schema/loginSchema'
 import MainLogo from '@/assets/img/logo/floli.svg'
 import { useToastMessageContext } from '@/providers/ToastMessageProvider'
 import { useAuthStore } from '@/store/useAuthStore'
+import { AuthResponse } from '@supabase/supabase-js'
 
 export function Login() {
   const { setUser } = useAuthStore()
@@ -20,6 +21,49 @@ export function Login() {
     formState: { errors, isSubmitting }
   } = useForm<LoginFormInputs>({ resolver: zodResolver(loginSchema) })
 
+  // Kakao 로그인 처리 함수
+  const handleKakaoLogin = async () => {
+    try {
+      const { data: sessionData, error } = await supabase.auth.signInWithOAuth({
+        provider: 'kakao',
+        options: {
+          redirectTo:
+            'https://vpxgcvxodypztnxelmqx.supabase.co/auth/v1/callback'
+        }
+      })
+
+      if (error) {
+        showToastMessage({
+          message: '카카오 로그인 실패하였습니다',
+          type: 'error'
+        })
+        console.error('Kakao Login error:', error)
+        return
+      }
+      const { data: session } = await supabase.auth.getSession()
+      if (session?.session?.user) {
+        setUser({
+          id: session.session.user.id,
+          email: session.session.user.email || '',
+          nickname: session.session.user.user_metadata?.name || '',
+          profile_img: session.session.user.user_metadata?.avatar_url || ''
+        })
+        console.log('Zustand User State (Kakao):', session.session.user)
+
+        showToastMessage({
+          message: '카카오 로그인 성공하였습니다',
+          type: 'success'
+        })
+        navigate('/')
+      }
+    } catch (error) {
+      showToastMessage({
+        message: '카카오 로그인 중 오류가 발생했습니다',
+        type: 'error'
+      })
+      console.error('Kakao Login error:', error)
+    }
+  }
   const handleLogin: SubmitHandler<LoginFormInputs> = async data => {
     const { email, password } = data
 
@@ -98,7 +142,7 @@ export function Login() {
           width="100%"
           fontSize="1.8rem"
           backgroundColor="var(--color-border)"
-          onClick={() => alert('구글 로그인')}
+          onClick={handleKakaoLogin}
           disabled={isSubmitting}
           bordertype={'기본'}>
           구글로 로그인
