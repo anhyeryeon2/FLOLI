f262740dc6133328a9ad9ff410f4c087e30d6101
KwonSeami <kwonsam07@daum.net>
2025-01-09 16:24:04 +0900

    누락된 머지 파일들 추가

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { getAuthStorage } from '@/repository/authRepository'
 
 const BASE_URL = import.meta.env.VITE_SUPABASE_URL
 const PROJECT_API_KEY = import.meta.env.VITE_SUPABASE_KEY
@@ -11,4 +12,23 @@ const axiosInstance = axios.create({
   }
 })
 
+axiosInstance.interceptors.request.use(
+  async config => {
+    const INITIAL_TOKEN = 'accessToken'
+    // const INITIAL_REFRESH_TOKEN = 'refreshToken'
+    const getToken = getAuthStorage(INITIAL_TOKEN)
+    // const getTokenfresh = getAuthStorage(INITIAL_REFRESH_TOKEN)
+    const token = getToken
+
+    if (token !== null) {
+      config.headers.Authorization = `Bearer ${token}`
+      // config.headers['Refresh-Token'] = `Bearer ${getTokenfresh}`
+    }
+    return config
+  },
+  async error => {
+    console.error('에러발생', error)
+    return Promise.reject(error)
+  }
+)
 export default axiosInstance
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ToastMessageProvider } from './ToastMessageProvider'
-import { OverlayProvider } from '@toss/use-overlay'
 
 const queryClient = new QueryClient()
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <OverlayProvider>
-        <ToastMessageProvider>{children}</ToastMessageProvider>
-      </OverlayProvider>
+      <ToastMessageProvider>{children}</ToastMessageProvider>
     </QueryClientProvider>
   )
 }
