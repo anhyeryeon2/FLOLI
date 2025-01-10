2a3d6076f1080087089b5215d92c7968f9e2736a
KwonSeami <kwonsam07@daum.net>
2025-01-10 15:41:18 +0900

    fix: user id 타입 수정 #66

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 
 interface User {
-  id: string | number
+  id: string
   email: string
   nickname: string
   profile_img: string
