93e30ea7fc77acbf0bb8830c78e891170d8f96f9
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-01-13 19:06:52 +0900

    feat: 유저 프로필 폴더 변경 #95

--- src/pages/userProfile/index.tsx
+++ src/pages/userProfile/index.tsx
@@ -1,12 +1,44 @@
+import { userInfoGet } from '@/apis/userInfoApi'
+import UserProfileList from '@/component/MyPageType/UserProfileList'
+import Profile from '@/component/Profile/Profile'
+import * as S from '@/pages/mypage/Mypage.styled'
+import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 
 export function UserProfile() {
-  const { userId } = useParams()
+  const { userId } = useParams<{ userId: string }>()
+
+  const { data } = useQuery({
+    queryKey: ['user', userId],
+    queryFn: () => userInfoGet(userId)
+  })
 
   return (
-    <div>
-      <h1>사용자 프로필</h1>
-      <p>선택한 사용자 ID: {userId}</p>
-    </div>
+    <S.Container>
+      <S.HeaderBox>
+        <S.ProfileBox>
+          <Profile
+            className="profile-img"
+            size="8rem"
+            userId="userIdtest"
+            imageUrl={data?.[0]?.profile_img}
+          />
+          <S.ProfileDetailBox>
+            <S.UserName>{data?.[0]?.nickname}</S.UserName>
+            <S.SubscribeCount>{data?.[0]?.subsc_count} 구독자</S.SubscribeCount>
+          </S.ProfileDetailBox>
+        </S.ProfileBox>
+        <S.ButtonBox></S.ButtonBox>
+      </S.HeaderBox>
+      <S.IntruductionBox>{data?.[0]?.introduction}</S.IntruductionBox>
+
+      <S.SeparatingBox>
+        <S.ShowTypes>플레이리스트</S.ShowTypes>
+      </S.SeparatingBox>
+
+      <S.PlayListsBox>
+        <UserProfileList />
+      </S.PlayListsBox>
+    </S.Container>
   )
 }
