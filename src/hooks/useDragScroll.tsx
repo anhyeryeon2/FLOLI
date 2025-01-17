ae59ade2fc9631feed9baf20ebc55aff73c545e7
rondido <rppr001@naver.com>
2025-01-17 12:46:36 +0900

    bug: 드래그 슬라이더 모바일 화면 작동 안되는 에러 해결

--- src/hooks/useDragScroll.tsx
+++ src/hooks/useDragScroll.tsx
@@ -1,15 +1,35 @@
 import { useState } from 'react'
 
+// 마우스와 터치 이벤트 타입을 각각 분리하여 처리
 export const useDragScroll = (scrollRef: React.RefObject<HTMLDivElement>) => {
   const [isDrag, setIsDrag] = useState(false)
   const [startX, setStartX] = useState(0)
-  const [_, setScrollLeft] = useState(0)
+  const [, setScrollLeft] = useState(0)
 
-  const onDragStart = (e: React.MouseEvent) => {
+  const getPageX = (e: MouseEvent | TouchEvent) => {
+    if ('touches' in e) {
+      return e.touches[0].pageX // 터치 이벤트에서 X 좌표
+    } else {
+      return e.pageX // 마우스 이벤트에서 X 좌표
+    }
+  }
+
+  // onDragStart는 MouseEvent, TouchEvent에 대해 각각 처리
+  const onMouseDown: React.MouseEventHandler<HTMLDivElement> = e => {
+    e.preventDefault() // preventDefault 호출 가능
+    setIsDrag(true)
+    const scrollLeft = scrollRef?.current?.scrollLeft || 0
+    setStartX(getPageX(e.nativeEvent) + scrollLeft) // nativeEvent를 통해 MouseEvent로 전달
+    setScrollLeft(scrollLeft)
+  }
+
+  const onTouchStart: React.TouchEventHandler<HTMLDivElement> = (
+    e: React.TouchEvent<HTMLDivElement>
+  ) => {
     e.preventDefault()
     setIsDrag(true)
     const scrollLeft = scrollRef?.current?.scrollLeft || 0
-    setStartX(e.pageX + scrollLeft)
+    setStartX(getPageX(e.nativeEvent) + scrollLeft)
     setScrollLeft(scrollLeft)
   }
 
@@ -17,12 +37,14 @@ export const useDragScroll = (scrollRef: React.RefObject<HTMLDivElement>) => {
     setIsDrag(false)
   }
 
-  const onDragMove = (e: React.MouseEvent) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onDragMove = (e: any) => {
     if (isDrag && scrollRef.current) {
       const scrollWidth = scrollRef.current.scrollWidth
       const clientWidth = scrollRef.current.clientWidth
-      scrollRef.current.scrollLeft = startX - e.pageX
+      scrollRef.current.scrollLeft = startX - getPageX(e)
 
+      // 스크롤이 0보다 작은 값으로 이동하지 않도록 제한
       if (scrollRef.current.scrollLeft < 0) {
         scrollRef.current.scrollLeft = 0
       } else if (scrollRef.current.scrollLeft > scrollWidth - clientWidth) {
@@ -34,14 +56,16 @@ export const useDragScroll = (scrollRef: React.RefObject<HTMLDivElement>) => {
   const onThrottleDragMove = throttle(onDragMove, 16) // throttle 적용
 
   return {
-    onDragStart,
+    onMouseDown,
+    onTouchStart,
     onDragEnd,
     isDrag,
     onThrottleDragMove
   }
 }
 
-function throttle<T extends (...args: any[]) => void>( // eslint-disable-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function throttle<T extends (...args: any[]) => void>(
   func: T,
   ms: number
 ): (...args: Parameters<T>) => void {
